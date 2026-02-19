75f57ee2d8c53ccf14000e2b020cda7bbe023eec
NoeŁ <124210421+vNoeLm@users.noreply.github.com>
2026-02-20 00:43:40 +0100

    Week1

---
+++ Week1/Week1/Cage.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week1
+{
+    internal class Cage
+    {
+        Animal[] animals;
+        int CurrentAnimalCount = 0;
+
+        public Cage(int size)
+        {
+            this.animals = new Animal[size];
+        }
+
+        public bool Add(Animal animal)
+        {
+            if (CurrentAnimalCount < animals.Length)
+            {
+                animals[CurrentAnimalCount] = animal;
+                CurrentAnimalCount++;
+                return true;
+            }
+            return false;
+        }
+
+        public void Delete(string name)
+        {
+            for (int i = 0; i < CurrentAnimalCount; i++)
+            {
+                if (animals[i].Name == name)
+                {
+                    animals[i] = animals[CurrentAnimalCount - 1];
+                    animals[CurrentAnimalCount - 1] = null;
+                    CurrentAnimalCount--;
+                }
+            }
+        }
+
+        public int HowManyGivenSpecies(Species species)
+        {
+            int count = 0;
+            for (int i = 0; i < CurrentAnimalCount; i++)
+            {
+                if (animals[i].Species == species)
+                {
+                    count++;
+                }
+                
+            }
+            return count;
+        }   
+
+        public bool IsThereGivenSpeciesAndGender(Species species, bool gender)
+        {
+            for (int i = 0; i < CurrentAnimalCount; i++)
+            {
+                if (animals[i].Species == species && animals[i].Gender == gender)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public Animal[] WhichAreTheGivenSpecies(Species species)
+        {
+            Animal[] ret = new Animal[CurrentAnimalCount];
+            int index = 0;
+            for (int i = 0; i < CurrentAnimalCount; i++)
+            {
+                if (animals[i].Species == species)
+                {
+                    ret[index] = animals[i];
+                    index++;
+                }
+            }
+            Array.Resize(ref ret, index);
+            return ret;
+        }
+
+        public double AverageWeightOfGivenSpecies(Species species)
+        {
+            int count = 0;
+            int weightSum = 0;
+            for (int i = 0; i < CurrentAnimalCount; i++)
+            {
+                if (animals[i].Species == species)
+                {
+                    count++;
+                    weightSum += animals[i].Weight;
+                }
+            }
+            if (count == 0)
+            {
+                return 0;
+            }
+            return (double)weightSum / count;
+        }
+
+        public bool InvertedCouple()
+        {
+            for (int i = 0; i < CurrentAnimalCount - 1; i++)
+            {
+                for (int j = i + 1; j < CurrentAnimalCount; j++)
+                {
+                    if (animals[i].Species == animals[j].Species && animals[i].Gender != animals[j].Gender)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static Cage GetCageWithMostSpecies(Cage[] cages, Species specie)
+        {
+            Cage BestCage = null;
+            int maxCount = 0;
+
+            foreach (var cage in cages)
+            {
+                int count = cage.HowManyGivenSpecies(specie);
+                if (count > maxCount)
+                {
+                    maxCount = count;
+                    BestCage = cage;
+                }
+            }
+            return BestCage;
+        }
+
+        override public string ToString()
+        {
+            string ret = $"Cage with {CurrentAnimalCount} animals:\n";
+            foreach (var animal in animals)
+            {
+                if (animal != null)
+                {
+                    ret += "\t" + animal.ToString() + "\n";
+                }
+            }
+            return ret;
+        }
+    }
+}
---
+++ Week1/Week1/Program.cs
@@ -0,0 +1,61 @@
+﻿namespace Week1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cage cage = new Cage(10);
+            Animal animal = new Animal("Doggo", true, 20, Species.Rabbit);
+            Animal animal1 = new Animal("Cukker", false, 100, Species.Panda);
+            Animal animal2 = new Animal("Pani", true, 5, Species.Panda);
+            Animal animal3 = new Animal("Doggy", false, 25, Species.Dog);
+            cage.Add(animal);
+            cage.Add(animal1);
+            cage.Add(animal2);
+            cage.Add(animal3);
+
+            Cage cage2 = new Cage(10);
+            Animal animal4 = new Animal("Doggo2", true, 22, Species.Dog);
+            Animal animal5 = new Animal("Cukker2", false, 110, Species.Panda);
+            Animal animal6 = new Animal("Pandy", true, 6, Species.Panda);
+            Animal animal7 = new Animal("Doggy2", false, 27, Species.Panda);
+            cage2.Add(animal4);
+            cage2.Add(animal5);
+            cage2.Add(animal6);
+            cage2.Add(animal7);
+
+
+            Console.WriteLine($"Original Cage:");
+            Console.WriteLine(cage);
+            Console.WriteLine($"----------------------------------------------------------------");
+
+            Console.WriteLine($"After deleting Panda:");
+            cage.Delete("Panda");
+            Console.WriteLine(cage);
+            Console.WriteLine($"----------------------------------------------------------------");
+
+            Console.WriteLine($"How many dogs in the cage: {cage.HowManyGivenSpecies(Species.Dog)}");
+            Console.WriteLine($"----------------------------------------------------------------");
+
+            Console.WriteLine($"Is there a male rabbit in the cage: {(cage.IsThereGivenSpeciesAndGender(Species.Rabbit, true) ? "Yes" : "No")}");
+            Console.WriteLine($"----------------------------------------------------------------");
+
+            Console.WriteLine($"Which animals are dogs in the cage:");
+            Animal[] dogs = cage.WhichAreTheGivenSpecies(Species.Dog);
+            foreach (var dog in dogs)
+            {
+                Console.WriteLine($"\t{dog}");
+            }
+            Console.WriteLine($"----------------------------------------------------------------");
+
+            Console.WriteLine($"Average weight of dogs in the cage: {cage.AverageWeightOfGivenSpecies(Species.Dog)}");
+            Console.WriteLine($"----------------------------------------------------------------");
+
+            Console.WriteLine($"Is there an inverted couple in the cage: {(cage.InvertedCouple() ? "Yes" : "No")}");
+            Console.WriteLine($"----------------------------------------------------------------");
+
+            Console.WriteLine($"Cage with most pandas: {Cage.GetCageWithMostSpecies(new Cage[] { cage, cage2 }, Species.Rabbit)}");
+            Console.WriteLine($"----------------------------------------------------------------");
+        }
+    }
+}
