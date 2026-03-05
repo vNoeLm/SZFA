6ac3d58183bc0d1cb380141f2da1c328d6fd0bc5
NoeŁ <vnoel05@gmail.com>
2026-03-05 15:55:17 +0100

    Week2

---
+++ Week2/ClassTask/ClassTask/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    internal class Circle : Shape
+    {
+        public float Radius { get; set; }
+        public Circle(float radius, Color color, bool isHoley) : base(color, isHoley)
+        {
+            Radius = radius;
+        }
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * Math.PI * Radius;
+        }
+    }
+}
---
+++ Week2/ClassTask/ClassTask/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask
+{
+    internal class Rectangle : Shape
+    {
+        public virtual double Width { get; set; }
+        public virtual double Height { get; set; }
+
+        public Rectangle(double width, double height, Color color, bool isHoley) : base(color, isHoley)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            string ret = $"Téglalapunk van\n{base.ToString()}";
+            return ret;
+        }
+
+        public override double Area()
+        {
+            return Width * Height;
+        }
+        public override double Perimeter()
+        {
+            return 2 * Width + 2 * Height;
+        }
+    }
+}
