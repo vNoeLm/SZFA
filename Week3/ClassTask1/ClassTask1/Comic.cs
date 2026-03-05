4017b1f012759d556a4c4c0939e85b7391ba41bd
NoeŁ <124210421+vNoeLm@users.noreply.github.com>
2026-03-05 13:28:38 +0100

    week3 task1

---
+++ Week3/ClassTask1/ClassTask1/Comic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask1
+{
+    internal class Comic : IComparable<Comic>
+    {
+        public int PageCount { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+
+        public int CompareTo(Comic? other)
+        {
+            return this.PageCount.CompareTo(other.PageCount);
+        }
+        override public string ToString()
+        {
+            return $"Title: {Title}, Author: {Author}, PageCount: {PageCount}";
+        }
+    }
+}
---
+++ Week3/ClassTask1/ClassTask1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ClassTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comic[] comics = new Comic[]
+            {
+                                new Comic { Author = "Stan Lee", Title = "Spider-Man", PageCount = 780 },
+                                new Comic { Author = "Frank Miller", Title = "The Dark Knight Returns", PageCount = 200 },
+                                new Comic { Author = "Alan Moore", Title = "Watchmen", PageCount = 550 }
+            };
+            Console.WriteLine("Befire Sorting: ");
+            foreach (var comic in comics)
+            {
+                Console.WriteLine(comic);
+            }
+            Console.WriteLine("---------------------------------");
+            Array.Sort(comics);
+            Console.WriteLine("After Sorting: ");
+            foreach (var comic in comics)
+            {
+                Console.WriteLine(comic);
+            }
+        }
+    }
+}
